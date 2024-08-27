8c65e0f5612b816ea0815524d3a17192422b3503
Lorenzo Cortese <129780161+TheDeadGuy77@users.noreply.github.com>
2024-08-27 16:15:35 +0200

    Update app.ts

--- backend/app.ts
+++ backend/app.ts
@@ -16,7 +16,12 @@ const app: Express = express();
 
 
 // Middleware to parse form data
-app.use(cors());
+app.use(cors(
+    origin: 'https://provasite.netlify.app',
+    methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
