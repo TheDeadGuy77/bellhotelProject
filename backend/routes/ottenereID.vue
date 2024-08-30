db662b81af379158db21d73af66ea3a4be849dcc
Luca Moretto <129780166+Luke48ita@users.noreply.github.com>
2024-08-30 09:47:14 +0200

    Update ottenereID.vue

--- backend/routes/ottenereID.vue
+++ backend/routes/ottenereID.vue
@@ -1,1 +1,7 @@
+import express from 'express';
+import Hotel from '../models/hotel';
+import Stanza from '../models/stanza';
+import Prenotazione from '../models/prenotazione'
+import mongoose from 'mongoose';
+
 
